4b67104cdda6513e8f83b9b41dcef0be8df31451
Aleks-Buruchenko <79582322+Aleks-Buruchenko@users.noreply.github.com>
2025-12-13 17:39:35 +0300

    Update Builder.scala

--- src/main/scala/compact/fp/logic/Builder.scala
+++ src/main/scala/compact/fp/logic/Builder.scala
@@ -4,9 +4,18 @@ import compact.fp.model._
 
 object Builder {
 
-  def buildConfiguration(components: List[Component]): Either[String, Configuration] =
-    if (Compatibility.allCompatible(components))
-      Right(Configuration(components))
-    else
+  def buildConfiguration(components: List[Component]): Either[String, Configuration] = {
+    if (components.isEmpty) {
+      Left("Не введено ни одного компонента.")
+    }
+    else if (!components.exists(_.compType == Motherboard)) {
+      Left("В конфигурации отсутствует материнская плата — сборка невозможна.")
+    }
+    else if (!Compatibility.allCompatible(components)) {
       Left("Найдены несовместимые компоненты.")
+    }
+    else {
+      Right(Configuration(components))
+    }
+  }
 }
